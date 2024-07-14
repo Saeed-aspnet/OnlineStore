32a9b3136774d64d4c29baaa6d740e0e799e944d
Saeed Hamidnejad <54764021+Saeed-aspnet@users.noreply.github.com>
2024-07-14 10:12:55 +0330

    Update SwaggerExtentions.cs

--- OnlineStore.WebApi/Extensions/SwaggerExtentions.cs
+++ OnlineStore.WebApi/Extensions/SwaggerExtentions.cs
@@ -35,34 +35,7 @@ public static class SwaggerExtentions
             setup.GroupNameFormat = "'v'VVV";
             setup.SubstituteApiVersionInUrl = true;
         });
-        services.AddSwaggerGen(setup =>
-        {
-            setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Name = "Authorization",
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
-                BearerFormat = "JWT",
-                Description = "Input your Bearer token in this format - Bearer {your token here} to access this API",
-            });
-            setup.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer",
-                        },
-                        Scheme = "Bearer",
-                        Name = "Bearer",
-                        In = ParameterLocation.Header,
-                    }, new List<string>()
-                },
-            });
-        });
+        services.AddSwaggerGen();
         services.ConfigureOptions<ConfigureSwaggerOptions>();
 
         return services;
